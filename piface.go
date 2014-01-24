d63be1f1c60fcd851f97d58d1132d6ad589f3d88
Luis Mesas <luismesas@gmail.com>
2014-01-25 00:33:20 +0100

    spi

---
+++ piface.go
@@ -0,0 +1,5 @@
+package main
+
+type PiFace struct{
+	
+}
--- spi.go
+++ spi.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"bytes"
 )
 
 const SPIDEV = "/dev/spidev"
@@ -15,17 +16,15 @@ const DEFAULT_CHIP = 0
 type SPIDevice struct{
 	bus int // 0
 	chip_select int // 0
-	spi_callback *interface{} // nil
 	fd  *os.File // nil 
 	spi_device string
 }
 
 // An SPI Device at /dev/spi<bus>.<chip_select>.
-func NewSPIDevice(bus int, chip_select int, spi_callback *interface{}) *SPIDevice{
+func NewSPIDevice(bus int, chip_select int) *SPIDevice{
 	spi := new(SPIDevice)
 	spi.bus = bus
 	spi.chip_select = chip_select
-	spi.spi_callback = spi_callback
 	spi.fd = nil
 
 	spi_device := fmt.Sprintf("%s%d.%d", SPIDEV, spi.bus, spi.chip_select)
@@ -34,14 +33,16 @@ func NewSPIDevice(bus int, chip_select int, spi_callback *interface{}) *SPIDevic
 	return spi
 }
 
+// Opens SPI device
 func (spi *SPIDevice) open_fd(spi_device string){
 	var err error
-	spi.fd, err = os.OpenFile(spi_device, os.O_RDWR, os.ModePerm)
+	spi.fd, err = os.OpenFile(spi_device, os.O_RDWR|os.O_SYNC, 0)
 	if err != nil {
 		log.Fatalf("I can't see %s. Have you enabled the SPI module? (%s)", spi_device, SPI_HELP_LINK)
 	}
 }
 
+// Closes SPI device
 func (spi *SPIDevice) close_fd(){
 	err := spi.fd.Close()
 	if err != nil {
@@ -49,7 +50,28 @@ func (spi *SPIDevice) close_fd(){
 	}
 }
 
+// Sends bytes over SPI channel and returns []byte response
+func (spi *SPIDevice) Send(bytes_to_send []byte) []byte{
+	//sends command
+	count, err := spi.fd.Write(bytes)
+	if err != nil {
+		log.Fatalf("Error sending bytes", err)
+	}
+	fmt.Printf("sent %d bytes: %q\n", count, bytes_to_send)
+
+	data := make([]byte, 100)
+	ncount, err = spi.fd.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+	return data
+}
+
 func main(){
+	log.Println("SPI")
 	device := NewSPIDevice(DEFAULT_BUS, DEFAULT_CHIP, nil)
+	log.Println("spi open")
 	device.close_fd()
+	log.Println("spi close")
 }
