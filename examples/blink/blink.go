18361c4fcc20ca903164e6bcdd65b611d5b86c25
Luis Mesas <luismesas@gmail.com>
2014-01-25 19:23:28 +0100

    improved blink example

--- examples/blink/blink.go
+++ examples/blink/blink.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/luismesas/go-piface/pifacedigital"
 	"github.com/luismesas/go-piface/spi"
 	"time"
 )
 
 func main(){
-	pfd := pifacedigital.NewPiFaceDigital(spi.DEFAULT_HARDWARE_ADDR, spi.DEFAULT_BUS, spi.DEFAULT_CHIP, true)
+	pfd := pifacedigital.NewPiFaceDigital(spi.DEFAULT_HARDWARE_ADDR, spi.DEFAULT_BUS, spi.DEFAULT_CHIP, false)
+	
+	err := pfd.InitBoard()
+	if err != nil {
+		fmt.Printf("Error on init board: %s", err)
+	}
 
 	for{
 		pfd.Leds[7].Toggle()
