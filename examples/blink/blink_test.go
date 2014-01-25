2db0e38cb1f08f2808cf153dd9e7b91e770c06f7
Luis Mesas <luismesas@gmail.com>
2014-01-25 20:13:05 +0100

    fixed blink test

--- examples/blink/blink_test.go
+++ examples/blink/blink_test.go
@@ -10,7 +10,8 @@ func TestSome(t *testing.T){
 	var b byte
 	t.Errorf("Size of byte is %d", unsafe.Sizeof(b))
 
-	t.Errorf("Size of SpiIOcTransfer is %d", unsafe.Sizeof(SpiIOcTransfer{}))
+	t.Errorf("Size of SpiIOcTransfer is %d", unsafe.Sizeof(spi.SpiIOcTransfer{}))
 
-	t.Errorf("Value of SpiIOcMessage(1) is %d", SpiIOcMessage(1))
+	t.Errorf("Value of SpiIOcMessage(1) is %d", spi.SpiIOcMessage(1))
+	t.Errorf("Value of SpiIOcMessage(2) is %d", spi.SpiIOcMessage(2))
 }
