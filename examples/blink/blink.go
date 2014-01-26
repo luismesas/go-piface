854816d062d25ab6d2c85633a6afced393cc3219
Luis Mesas <luismesas@gmail.com>
2014-01-26 12:04:32 +0100

    blink example explanation

--- examples/blink/blink.go
+++ examples/blink/blink.go
@@ -8,16 +8,19 @@ import (
 )
 
 func main(){
+
+	// creates a new pifacedigital instance
 	pfd := piface.NewPiFaceDigital(spi.DEFAULT_HARDWARE_ADDR, spi.DEFAULT_BUS, spi.DEFAULT_CHIP)
 	
+	// initializes pifacedigital board
 	err := pfd.InitBoard()
 	if err != nil {
 		fmt.Printf("Error on init board: %s", err)
 		return
 	}
 
+	// blink time!!
 	fmt.Println("Blinking led 7 each second")
-
 	for{
 		pfd.Leds[7].Toggle()
 		time.Sleep(time.Second)
